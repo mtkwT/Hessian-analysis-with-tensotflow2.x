622181d799cdda669ddbb512582d9ad3f549e823
mtkwT <moto.t.03.td@gmail.com>
2020-04-25 07:26:56 +0900

    Small CNN for test

---
+++ models/small_cnn.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+from tensorflow.keras.layers import Dense, Flatten, Conv2D
+from tensorflow.keras import Model
+
+class SmallCNN(Model):
+    def __init__(self):
+        super(MyModel, self).__init__()
+        self.conv1 = Conv2D(32, 3, activation='relu')
+        self.flatten = Flatten()
+        self.d1 = Dense(128, activation='relu')
+        self.d2 = Dense(10)
+
+    def call(self, x):
+        x = self.conv1(x)
+        x = self.flatten(x)
+        x = self.d1(x)
+        return self.d2(x)
