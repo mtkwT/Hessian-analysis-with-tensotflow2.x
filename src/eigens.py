75efa902ceb08630b530445ab3ee88fb18292c05
mtkwT <moto.t.03.td@gmail.com>
2020-04-25 21:59:42 +0900

    Fix scipy URL

--- src/eigens.py
+++ src/eigens.py
@@ -4,7 +4,7 @@ def calculate_topK_eigens(hessian, k=2):
     """
     Because Hessian matrix is a real symmetric matrix,
     we should use "scypy.linalg.eigh" instead of "numpy.linalg.eig"
-    https://docs.scipy.org/doc/numpy/reference/generated/numpy.linalg.eigh.html
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.linalg.eigh.html
     """
     eig_val, eig_vec = linalg.eigh(hessian, eigvals=(len(hessian)-k, len(hessian)-1))
     return eig_val, eig_vec
