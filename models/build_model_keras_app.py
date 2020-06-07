f54f60e7154da7eaafaf620079b03fa11047eb89
mtkwT <moto.t.03.td@gmail.com>
2020-06-07 19:08:45 +0900

    Add trained model by keras.app

---
+++ models/build_model_keras_app.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+
+def build_densenet121(
+    input_shape,
+    output_dim,
+    is_train_feature_extractor_layer=True):
+    """
+    transfer learning using imagenet weights
+    """
+    feature_extractor_layer = \
+        tf.keras.applications.DenseNet121(
+            include_top=False, 
+            input_shape=input_shape, 
+            weights='imagenet', 
+            pooling='avg') 
+    feature_extractor_layer.trainable = is_train_feature_extractor_layer
+
+    model = tf.keras.Sequential([
+        feature_extractor_layer,
+        # If you are using CIFAR100, you will need to reduce the layer size.
+        tf.keras.layers.Dense(128,        activation='relu'),
+        tf.keras.layers.Dense(output_dim, activation='softmax')
+    ])
+    return model
